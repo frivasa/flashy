5aa637f1eaa57ffc9f2a5c10676755a93cbc22a1
Fernando Rivas <frivasa@users.noreply.github.com>
2018-10-18 21:10:14 -0400

    Create setup.py

---
+++ flashy/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='flashy',
+      version='0.4',
+      description='FLASH output analysis tools',
+      url='http://github.com/frivasa/flashy',
+      author='Fernando Rivas',
+      author_email='rivas.aguilera@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
