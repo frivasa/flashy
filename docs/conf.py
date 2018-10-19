84f5a1122f153bce4a3be6477fc5c2e50664ad16
Fernando Rivas <frivasa@users.noreply.github.com>
2018-10-18 21:37:45 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../flashy'))
 sys.path.insert(0, os.path.abspath('../flashy/plot'))
-
+sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = u'flashy'
