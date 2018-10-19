ec42c5d11923b3da4f5413ae85219a9b5906954f
frivasa <rivas.aguilera@gmail.com>
2018-10-18 21:03:50 -0400

    docs 0.5

---
+++ flashy/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flashy',
+      version='0.4',
+      description='FLASH output analysis tools',
+      url='http://github.com/frivasa/flashy',
+      author='Fernando Rivas',
+      author_email='rivas.aguilera@gmail.com',
+      license='MIT',
+      packages=['flashy'],
+      zip_safe=False)
