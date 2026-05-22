7ea616327c1f9e76be8d9f919b03433cbfdbae51
Fernando Rivas <rivas.aguilera@gmail.com>
2026-05-22 12:24:08 +1200

    paraMan docs and some case-testing

--- flashy/paraMan.py
+++ flashy/paraMan.py
@@ -1,23 +1,42 @@
 import functools
+from .IOutils import log
 
 
 def setupParaData(rc):
     balv = rc.load_balanced_view()
-    dirv = rc.direct_view()
-    with dirv.sync_imports():
-        pass
     return balv
 
 
 def setupParaPost(rc):
     balv = rc.load_balanced_view()
-    dirv = rc.direct_view()
-    with dirv.sync_imports():
-        pass
     return balv
 
 
-def throwHammer(balv, N, func, **kwargs):
+def throwHammer(balv, N, func, timeout=None, propagate_errors=False, **kwargs):
+    """Execute func in parallel across N workers.
+
+    Args:
+        balv: Load-balanced view from ipp
+        N: Number of parallel tasks
+        func: Function to execute (takes wedgenum as first arg)
+        timeout: Optional timeout in seconds
+        propagate_errors: If True, raise first exception; if False, log and continue
+        **kwargs: Arguments passed to func
+
+    Returns:
+        AsyncResult if timeout is None, otherwise the result of .get()
+    """
     kwargf = functools.partial(func, **kwargs)
     res = balv.map_async(kwargf, range(N))
-    return res
+
+    if timeout is None:
+        return res
+
+    try:
+        return res.get(timeout=timeout)
+    except Exception as e:
+        log.critical('Parallel task failed: {}: {}'.format(type(e).__name__, e),
+                     exc_info=True)
+        if propagate_errors:
+            raise
+        return None
